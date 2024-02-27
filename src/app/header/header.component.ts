4d023059539a122c23c373c934bf83354b05e184
Vaishnav Ghangale <107029372+vaishnav196@users.noreply.github.com>
2024-02-27 17:47:28 +0530

    Update header.component.ts

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -12,21 +12,28 @@ export class HeaderComponent {
 constructor(private loginService:LoginService,private router:Router){
   let token =localStorage.getItem('token')
   if(token){
-    this.loginService.islogged=true
+    this.loginService.islogged=true;
+    this.loginService.isAdminLoged=true;
   }
 }
 
-isAdminLogged(){
-  
+isAdminLogged():boolean{
+
+  return this.loginService.isAdminLoged = false;
 }
 
  isloggedIn():boolean{
   return this.loginService.isLoggedIn();
  }
+admin(){
 
+   return this.loginService.isAdmin();
+    // return this.loginService.isAdminLoged=true;
+}
 
 logout(){
 this.router.navigateByUrl("/Login")
+this.loginService.isAdminLoged=false;
   return this.loginService.logout();  
 }
 
